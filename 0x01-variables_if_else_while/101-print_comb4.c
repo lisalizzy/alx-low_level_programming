89e8ee89286b0d7b0162c50ccfa4ccc5a55ee3a2
lisalizzy <lisaaladete@gmail.com>
2023-02-17 16:16:53 -0800

    101 print comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,27 +1,42 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - is entry of program
  *
- * Return: Always 0 (Success)
+ * Return: 0, if succesful
  */
 
 int main(void)
 {
-int nums;
-int nums1;
-
-for (nums = '0'; nums <= '9'; nums++)
+/*
+ * prints all combination of 3 different digits once,in accending order
+ * after each combination follows a coma and a space
+ */
+	
+/* take abc as possible digits */
+int a, b, c;
+	
+/*loop through a,1st digit,lowest of the 3, highest possible is 7*/
+for (a = 0 ; a <= 7 ; a++)
 {
-for (nums1 = '0'; nums1 <= '9'; nums1++)
+/*loop through b,2nd digit,higher than a by 1,highest possible is 8*/
+for (b = a + 1 ; b <= 8 ; b++)
 {
-putchar(nums);
-putchar(nums1);
-if (nums == '9' && nums1 == '9')
+/*loop through c,3rd digit,higher than b by 1,highest possible is 9*/
+for (c = b + 1 ; c <= 9 ; c++)
+{
+putchar((a % 10) + '0');
+putchar((b % 10) + '0');
+putchar((c % 10) + '0');
+/*continues to put coma,space,stop if last highest combination is met*/
+if (a == 7 && b == 8 && c == 9)
 continue;
 putchar(',');
 putchar(' ');
 }
 }
+}
 putchar('\n');
 return (0);
 }
+	
+
